4bd737167b82547d3ee6b7ee2de390b268731d7b
lordcholotloc66 <Quetzalcoat@Marios-MacBook-Pro.local>
2017-02-15 18:28:57 -0800

    math

---
+++ exercisemath.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var str1 = "Math push to git hub"
+
+var age = 15 //1
+var age1 = 5 ///2
+var age2 = age + age1 //3
+var age3 = age * 3 ////4
+var age4 = age - age1 //5
+var age5 = age / age1 //6
+var age7 = 1, age8 = 2, age9 = 3 //7
+let old = 7.5, old1 = 3.5,old2 = "Good bye" //8
+var age10: Double ///9
+var age11: Int = 423 //10
+var mess = "Hello" //11 String
+var mess1 = "Word" //12 String
+var mess2 = mess+" "+mess1 //13 Concatenating
+var mess3 = "\(mess) \(mess1)" //14 concatenating and interpolation
+
+
+
+
+///*1.Create a variable with a name of your choosing that has a value of 15
+//2.Create a 2nd variable with a name of your choosing that has a value of 5
+//3.Create a 3rd variable that stores that sum of the 1st and 2nd variables (20 should print to the screen)
+//4.Create a 4th variable that stores the result of a multiplication operation - the stored result should be equal to 45
+//5.Create a 5th variable that stores the result of the 1st variable minus the 2nd variable - the result should be 10
+//6.Create a 6th variable that stores the result of the 1st variable divided by the 2nd variable - the result should be 3
+//7.Create 3 new variables of any name and of any value, but all on the same line
+//8.Create 3 constants (let) of any name and of any value, but all on the same line
+//9.Create an explicit variable of any name of type Double and don't give it any value
+//10.Create another explicit variable of any name of type Int but DO give it an initial value of 423
+//11.Create a variable of type String with a value of "Hello"
+//12.Create another variable of type String with a value of "World"
+//13.Create a 3rd variable to store the result of concatenating the first two strings (using the + symbol)
+//14.Using that 3rd variable again, store the result of concatenating the first two string but instead using "(someVar)" syntax (string interpolation)
+
+
+
+
+
+
+
+
+
+
+
+
