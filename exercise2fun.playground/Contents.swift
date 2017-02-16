bc60ab43fb7269ed44b5725cb0364b9aee99c48e
lordcholotloc66 <Quetzalcoat@Marios-MacBook-Pro.local>
2017-02-15 18:04:55 -0800

    func
    I add a line on 6

---
+++ exercise2fun.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var str2 = "My Exercise #2  OF FUNC "
+
+
+func add(apple1: Double, apple2: Double) -> Double { ///2
+    return apple1 + apple2
+    
+}
+
+func substract(apple3: Int, apple4: Int) -> Int { ///3
+    return apple3 - apple4
+    
+}
+
+
+func multiply(apple5: Float, apple6: Float) -> Float { ///4
+    return apple5 * apple6
+    
+}
+
+func devede(apple7: Double, apple8: Double) -> Double { ///5
+    return apple7 / apple8
+    
+}
+
+var add1 = add(apple1: 10,   apple2: 6) //////6
+var subst2 = substract(apple3: 10, apple4: 6)
+var mult3 = multiply(apple5: 10, apple6: 6)
+var dev4 = devede(apple7: 10, apple8: 6)
+
+
+
+
+
+
+
